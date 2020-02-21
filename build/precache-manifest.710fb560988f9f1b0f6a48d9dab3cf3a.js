0764dab05c2c4ba6e3ccd64638a5b2fbd4812157
Sinkin Oleg <applesinkin@gmail.com>
2020-02-21 16:00:03 +0200

    add (completed build) add build temporary

---
+++ build/precache-manifest.710fb560988f9f1b0f6a48d9dab3cf3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0929c55058b5f7dba884378c9dc077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27261b1bec0798baf5b",
+    "url": "/static/css/main.14fa8207.chunk.css"
+  },
+  {
+    "revision": "cbd01e90bfee2467fc64",
+    "url": "/static/js/2.c46abfa9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.c46abfa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27261b1bec0798baf5b",
+    "url": "/static/js/main.6d3f107e.chunk.js"
+  },
+  {
+    "revision": "9a8dbbc30754b4e916fe",
+    "url": "/static/js/runtime-main.28f23b24.js"
+  }
+]);
